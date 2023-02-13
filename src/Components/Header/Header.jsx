1d6920129d04446bb5ba2c6b86090593aa352385
Recep Terzi <recepterzi-67@hotmail.com>
2023-02-13 19:00:56 +0300

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.Module.css";
 import { FaFacebookF } from "react-icons/fa";
 import { SlSocialInstagram } from "react-icons/sl";
 import { BsTwitter } from "react-icons/bs";
 import { ImSearch } from "react-icons/im";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Header = ({ search, setSearch, setSearchValue }) => {
   const [category, setCategory] = useState();
+  const hamburgerOpen = useRef(null);
+  const hamburgerClose = useRef(null);
+  const mobileNav = useRef(null);
   const handleSubmit = (e) => {
     e.preventDefault();
     setSearchValue(search);
@@ -27,15 +31,32 @@ const Header = ({ search, setSearch, setSearchValue }) => {
       document.getElementById("navbar-section").style.position = "relative";
     }
   };
+  useEffect(() => {
+    const openHamburger = hamburgerOpen.current;
+    openHamburger.addEventListener("click", () => {
+      const navMobile = mobileNav.current;
+      navMobile.classList.add("open");
+    });
+  }, []);
+  useEffect(() => {
+    const closeHamburger = hamburgerClose.current;
+    closeHamburger.addEventListener("click", () => {
+      const navMobile = mobileNav.current;
+      navMobile.classList.remove("open");
+    });
+  }, []);
   return (
     <>
       <div className="container">
         <header id="header-section">
           <div className="header-logo">
             <Link to="/"> Recep Terzi </Link>
+            <div ref={hamburgerOpen}>
+              <AiOutlineMenu />
+            </div>
           </div>
           <div className="header-menu">
-            <FaFacebookF />
+            <FaFacebookF className="facebook" />
             <BsTwitter />
             <SlSocialInstagram />
             <form onSubmit={handleSubmit}>
@@ -51,6 +72,88 @@ const Header = ({ search, setSearch, setSearchValue }) => {
             </form>
           </div>
         </header>
+      </div>
+      <div className="mobile-navbar" ref={mobileNav}>
+        <div ref={hamburgerClose}>
+          <AiOutlineClose />
+        </div>
+        <div className="mobile-nav-link">
+          <Link
+            to="/"
+            onClick={() => setCategory("Home")}
+            className={category === "Home" ? "active-header" : "head-link"}
+          >
+            Home
+          </Link>
+
+          <Link
+            to="/allnews"
+            onClick={() => setCategory("Categories")}
+            className={
+              category === "Categories" ? "active-header" : "head-link"
+            }
+          >
+            Categories
+          </Link>
+          <Link
+            to="/politics"
+            onClick={() => setCategory("Politics")}
+            className={category === "Politics" ? "active-header" : "head-link"}
+          >
+            Politics
+          </Link>
+          <Link
+            to="/business"
+            onClick={() => setCategory("Business")}
+            className={category === "Business" ? "active-header" : "head-link"}
+          >
+            Business
+          </Link>
+          <Link
+            to="/health"
+            onClick={() => setCategory("Health")}
+            className={category === "Health" ? "active-header" : "head-link"}
+          >
+            Health
+          </Link>
+          <Link
+            to="/technology"
+            onClick={() => setCategory("Technology")}
+            className={
+              category === "Technology" ? "active-header" : "head-link"
+            }
+          >
+            Technology
+          </Link>
+          <Link
+            to="/sports"
+            onClick={() => setCategory("Sports")}
+            className={category === "Sports" ? "active-header" : "head-link"}
+          >
+            Sports
+          </Link>
+          <Link
+            to="/science"
+            onClick={() => setCategory("Science")}
+            className={category === "Science" ? "active-header" : "head-link"}
+          >
+            Science
+          </Link>
+          <Link
+            to="/contact"
+            onClick={() => setCategory("Contact")}
+            className={category === "Contact" ? "active-header" : "head-link"}
+          >
+            Contact
+          </Link>
+          <Link
+            to="/about"
+            onClick={() => setCategory("about")}
+            className={category === "about" ? "active-header" : "head-link"}
+          >
+            About
+          </Link>
+        </div>
       </div>
       <navbar id="navbar-section">
         <div className="container">
